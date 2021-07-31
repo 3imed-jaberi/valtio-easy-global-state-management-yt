6d0c676821a313e9c08859ef6efc9323fb23b647
3imed-jaberi <imed_jebari@hotmail.fr>
2021-07-31 02:18:18 +0200

    add main component --app 🎯

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Counter from './components/Counter'
+import Timers from './components/Timers'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Timers />
+      <Counter />
+    </div>
+  )
+}
+
+export default App
